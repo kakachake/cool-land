67a91849adce8d65300b3e04f51f451b3d9c6b33
kakachake <kakachake@users.noreply.github.com>
2023-05-20 14:05:13 +0000

    deploy: dbfae2d91a4a02cb82f801b94c849f4cbfa52ba9

---
+++ assets/index-0e8171e1.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-3b88282b.js";function a(){return t.jsx(t.Fragment,{children:t.jsx(r,{})})}export{a as default};
--- assets/index-14a99aed.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-133b59ef.js";function a(){return t.jsx(t.Fragment,{children:t.jsx(r,{})})}export{a as default};
